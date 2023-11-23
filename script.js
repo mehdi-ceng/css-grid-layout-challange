17cebf27131b8c693b4dd49cd8da34b3b6ebc615
mehdi-ceng <mehdi.maharramov1@gmail.com>
2023-11-23 15:58:13 +0400

    Final touches

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function toggleNav() {
   const outerGrid = document.querySelector(".outer-grid");
   outerGrid.classList.toggle("outer-grid-changed");
 
-  const nav = document.querySelector(".nav");
+  const nav = document.querySelector("nav");
   nav.classList.toggle("hide-nav");
 }
